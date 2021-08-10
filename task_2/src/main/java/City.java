6b43e107bac8607d9def4624e97988d3c66ccfb1
Igor <igos_96@mail.ru>
2021-08-10 13:30:25 +0300

    task_2

---
+++ task_2/src/main/java/City.java
@@ -0,0 +1,42 @@
+public class City {
+    private String name;
+    private String region;
+    private String district;
+    private int population;
+    private String foundation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public City(String name, String region, String district, int population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation=" + foundation +
+                '}';
+    }
+}
---
+++ task_2/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main extends CityUtils {
+    public static void main(String[] args) {
+        sortingPrintByOneParameters(parse());
+        System.out.println("\n\t\t\t***\n ");
+        sortingPrintByTwoParameters(parse());
+    }
+}
